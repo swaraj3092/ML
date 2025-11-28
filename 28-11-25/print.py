622210f1d64cf9c84f13d2c6ccf9f2c01dc3061e
swaraj3092 <swarajbehera923@gmail.com>
2025-11-28 08:35:39 +0530

    uodate

--- 28-11-25/print.py
+++ 28-11-25/print.py
@@ -1,2 +1,14 @@
-# print.py
-print("hello world")
+import csv
+
+data = [
+    ['Name', 'Age', 'City','Mobile Number','rollno'],
+    ['Swaraj', 21, 'Puri', '1234567890','23053092'],
+    ['Bob', 26, ' Bihar', '0987654321','23052611'],
+    ['Prajakta', 20 , 'Tata', '1122334455','23052811']
+]
+
+with open('main.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows(data)
+
+print("CSV file 'main.csv' created successfully.")
